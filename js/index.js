3daaca687c99197e7d1d36600bd1e8d7ab5a6b45
Yoshiki Aoki <ringohub@gmail.com>
2017-07-13 02:14:27 +0900

    Started

--- js/index.js
+++ js/index.js
@@ -4,3 +4,63 @@ var app = new Vue({
     message: 'Hello Vue!'
   }
 });
+
+var app2 = new Vue({
+  el: '#app-2',
+  data: {
+    message: `Foo Bar ${new Date()}`
+  }
+});
+
+var app3 = new Vue({
+  el: '#app-3',
+  data: {
+    seen: true
+  }
+});
+
+var app4 = new Vue({
+  el: '#app-4',
+  data: {
+    todos: [
+      { text: 'foo' },
+      { text: 'bar' },
+      { text: 'baz' }
+    ]
+  }
+});
+
+var app4 = new Vue({
+  el: '#app-5',
+  data: {
+    message: 'Hello Vue!'
+  },
+  methods: {
+    reverseMessage: function() {
+      this.message = this.message.split('').reverse().join('');
+    }
+  }
+});
+
+var app4 = new Vue({
+  el: '#app-6',
+  data: {
+    message: 'Hello Vue!'
+  }
+});
+
+Vue.component('todo-item', {
+  props: ['todo'],
+  template: '<li>{{ todo.text }}</li>'
+});
+
+var app7 = new Vue({
+  el: '#app-7',
+  data: {
+    groceryList: [
+      { id: 0, text: 'Fooo' },
+      { id: 1, text: 'Bar' },
+      { id: 2, text: 'Buz' }
+    ]
+  }
+});
